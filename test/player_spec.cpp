3d12477bdb1c506dffe9baedd4053fcd4aae1dd0
Johannes Ruder <jruder@mytum.de>
2018-03-31 22:50:12 +0200

    Cleaned up includes with include-what-you-need.

--- test/player_spec.cpp
+++ test/player_spec.cpp
@@ -12,38 +12,38 @@
 class DefaultPlayerFixture : public ::testing::Test
 {
 protected:
-    Player unit{};
+    Player unit_{};
 };
 
 TEST_F(DefaultPlayerFixture, DefaultConstructor)
 {
-    EXPECT_EQ("", unit.GetName());
-    EXPECT_EQ(0, unit.GetNumber());
-    EXPECT_EQ(Position::UNDEFINED, unit.GetPosition());
-    EXPECT_EQ(0, unit.GetNumberOfActions());
+    EXPECT_EQ("", unit_.GetName());
+    EXPECT_EQ(0, unit_.GetNumber());
+    EXPECT_EQ(Position::UNDEFINED, unit_.GetPosition());
+    EXPECT_EQ(0, unit_.GetNumberOfActions());
 }
 
 TEST_F(DefaultPlayerFixture, IncrementNumberOfActions)
 {
-    EXPECT_EQ(0, unit.GetNumberOfActions());
+    EXPECT_EQ(0, unit_.GetNumberOfActions());
 
-    unit.IncrementNumberOfActions();
+    unit_.IncrementNumberOfActions();
 
-    EXPECT_EQ(1, unit.GetNumberOfActions());
+    EXPECT_EQ(1, unit_.GetNumberOfActions());
 }
 
 class ExamplePlayerFixture : public ::testing::Test
 {
 protected:
-    Player unit{"Hannes", 9, Position::MITTELBLOCKER};
+    Player unit_{"Hannes", 9, Position::MITTELBLOCKER};
 };
 
 TEST_F(ExamplePlayerFixture, Constructor)
 {
-    EXPECT_EQ("Hannes", unit.GetName());
-    EXPECT_EQ(9, unit.GetNumber());
-    EXPECT_EQ(Position::MITTELBLOCKER, unit.GetPosition());
-    EXPECT_EQ(0, unit.GetNumberOfActions());
+    EXPECT_EQ("Hannes", unit_.GetName());
+    EXPECT_EQ(9, unit_.GetNumber());
+    EXPECT_EQ(Position::MITTELBLOCKER, unit_.GetPosition());
+    EXPECT_EQ(0, unit_.GetNumberOfActions());
 }
 
 TEST_F(ExamplePlayerFixture, WritePlayerToFile)
@@ -51,40 +51,40 @@ TEST_F(ExamplePlayerFixture, WritePlayerToFile)
     static const char *const filename = "/home/hannes/workspace/data.yaml";
 
     cv::FileStorage file_storage(filename, cv::FileStorage::WRITE);
-    file_storage << "player" << unit;
+    file_storage << "player" << unit_;
     file_storage.release();
 
-    Player playerFromFile;
+    Player player_from_file;
     file_storage.open(filename,cv::FileStorage::READ);
-    file_storage["player"] >> playerFromFile;
+    file_storage["player"] >> player_from_file;
     file_storage.release();
 
-    EXPECT_EQ(unit.GetName(), playerFromFile.GetName());
-    EXPECT_EQ(unit.GetNumber(), playerFromFile.GetNumber());
-    EXPECT_EQ(unit.GetPosition(), playerFromFile.GetPosition());
-    EXPECT_EQ(unit.GetNumberOfActions(), playerFromFile.GetNumberOfActions());
+    EXPECT_EQ(unit_.GetName(), player_from_file.GetName());
+    EXPECT_EQ(unit_.GetNumber(), player_from_file.GetNumber());
+    EXPECT_EQ(unit_.GetPosition(), player_from_file.GetPosition());
+    EXPECT_EQ(unit_.GetNumberOfActions(), player_from_file.GetNumberOfActions());
 }
 
 TEST_F(ExamplePlayerFixture, ComparePlayers_AllEqual)
 {
     Player player{"Hannes",9,Position::MITTELBLOCKER};
-    EXPECT_TRUE(unit == player);
+    EXPECT_TRUE(unit_ == player);
 }
 
 TEST_F(ExamplePlayerFixture, ComparePlayers_DifferentName)
 {
     Player player{"Michi",9,Position::MITTELBLOCKER};
-    EXPECT_FALSE(unit == player);
+    EXPECT_FALSE(unit_ == player);
 }
 
 TEST_F(ExamplePlayerFixture, ComparePlayers_DifferentNumber)
 {
     Player player{"Hannes",6,Position::MITTELBLOCKER};
-    EXPECT_FALSE(unit == player);
+    EXPECT_FALSE(unit_ == player);
 }
 
 TEST_F(ExamplePlayerFixture, ComparePlayers_DifferentPosition1)
 {
     Player player{"Hannes",9,Position::AUSSENANGREIFER};
-    EXPECT_TRUE(unit == player);
+    EXPECT_TRUE(unit_ == player);
 }
